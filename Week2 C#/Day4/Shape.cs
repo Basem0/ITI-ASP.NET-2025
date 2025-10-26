79b253c777378593484a8b3e67b3afb4d253f426
Ahmed Basem <Ahmedbasem0104@gmail.com>
2025-10-27 00:22:19 +0300

    All Tasks

---
+++ Week2 C#/Day4/Shape.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Raylib_cs;
+
+namespace dayFour
+{
+    public abstract class Shape
+     {
+
+        public Shape() { }
+
+        public Raylib_cs.Color Color { get; set; }
+        public abstract void Draw();
+
+     }
+}
---
+++ Week3 EF Core/Day8/Heater.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dayEight
+{
+    public class Heater
+    {
+        public float Temp { get; set; }
+
+        public void TemperatureChanged(float newTemp)
+        {
+            if (newTemp < Temp)
+                Console.WriteLine($"[Heater] ON as Current Temp : {newTemp}");
+            else
+                Console.WriteLine($"[Heater] OFF as Current Temp: {newTemp}");
+        }
+    }
+}
